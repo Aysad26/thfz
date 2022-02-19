535e6b9d101fc3427d779e19234defdcf4bd3d62
Anton Sadyrin <aysad26@yandex.ru>
2022-02-19 20:46:12 +0300

    edit script

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
   const button = document.querySelector('.menu__burger-button');
   const aside = document.querySelector('.menu__nav')
-     
+
   button.addEventListener('click', (e) => {
     button.classList.toggle('menu__burger-button_active');
     aside.classList.toggle('menu__nav_active')
   });
 
 const counters = document.querySelectorAll('.number__title');
-console.log (counters);
 const speed = 96; // The lower the slower
 
 counters.forEach(counter => {
